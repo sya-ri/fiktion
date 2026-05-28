919320da42cdad2be5f84774761e7a3c112a7d27
sya-ri <git@s7a.dev>
2026-05-28 22:57:22 +0900

    Add addon installation coverage

--- fiktion-core/src/commonTest/kotlin/dev/s7a/fiktion/FiktionAddonBuilderTest.kt
+++ fiktion-core/src/commonTest/kotlin/dev/s7a/fiktion/FiktionAddonBuilderTest.kt
@@ -2,8 +2,38 @@ package dev.s7a.fiktion
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class FiktionAddonBuilderTest {
+    @Test
+    fun `add-on rules are not active until the add-on is installed`() {
+        assertFailsWith<CannotGenerateException> {
+            fake<AddonToken>(seed = 123)
+        }
+    }
+
+    @Test
+    fun `installed add-on contributes type rules`() {
+        val fiktion =
+            Fiktion {
+                install(ExampleAddon)
+            }
+
+        assertEquals(AddonToken("token-123"), fiktion.fake<AddonToken>(seed = 123))
+        assertEquals(AddonLabel("label-456"), fiktion.fake<AddonLabel>(seed = 456))
+    }
+
+    @Test
+    fun `instance rules override installed add-on rules`() {
+        val fiktion =
+            Fiktion {
+                install(ExampleAddon)
+                type<AddonToken>() generates AddonToken("instance-token")
+            }
+
+        assertEquals(AddonToken("instance-token"), fiktion.fake<AddonToken>(seed = 123))
+    }
+
     @Test
     fun `rule seed configured by an add-on is applied to the installed rule`() {
         val addon =
@@ -25,5 +55,28 @@ class FiktionAddonBuilderTest {
             }
 
         assertEquals(User(id = "user-456"), fiktion.fake<User>())
+    }
+
+    private data class AddonToken(
+        val value: String,
+    )
+
+    private data class AddonLabel(
+        val value: String,
+    )
+
+    private object ExampleAddon : FiktionAddon {
+        override val id: String = "test-example-addon"
+
+        override fun install(builder: FiktionAddonBuilder) {
+            with(builder) {
+                type<AddonToken>() generatesBy {
+                    AddonToken("token-$seed")
+                }
+                type<AddonLabel>() generatesBy {
+                    AddonLabel("label-$seed")
+                }
+            }
+        }
     }
 }
