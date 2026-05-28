c25e1b260b044c5a8b8e2d2443b96f2e2fc3c60e
sya-ri <git@s7a.dev>
2026-05-28 22:27:03 +0900

    Remove unused metadata module

--- fiktion-metadata/src/commonMain/kotlin/dev/s7a/fiktion/metadata/SchemaVersion.kt
+++
@@ -1,6 +0,0 @@
-package dev.s7a.fiktion.metadata
-
-/**
- * Metadata schema version expected by this runtime.
- */
-public const val FIKTION_METADATA_SCHEMA_VERSION: Int = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,6 @@ rootProject.name = "fiktion"
 include(
     ":fiktion-core",
     ":fiktion-runtime",
-    ":fiktion-metadata",
     ":fiktion-compiler-plugin",
     ":fiktion-gradle-plugin",
 )
