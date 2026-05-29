761fa963f528963dfbd5bd9d12eaa50e970dea6a
sya-ri <git@s7a.dev>
2026-05-29 11:58:58 +0900

    Add Kover coverage reporting

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import dev.detekt.gradle.extensions.DetektExtension
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kover)
     alias(libs.plugins.kotlinter) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.dokka)
@@ -15,20 +16,25 @@ group = "dev.s7a"
 version = "0.1.0"
 
 dependencies {
-    dokka(project(":fiktion-addon-java"))
-    dokka(project(":fiktion-compiler-plugin"))
-    dokka(project(":fiktion-core"))
-    dokka(project(":fiktion-gradle-plugin"))
+    subprojects.forEach { it ->
+        dokka(it)
+        kover(it)
+    }
 }
 
 subprojects {
+    apply(plugin = "org.jmailen.kotlinter")
+    apply(plugin = "dev.detekt")
+    apply(plugin = "org.jetbrains.kotlinx.kover")
+
     group = rootProject.group
     version = rootProject.version
 
-    plugins.withId("org.jetbrains.kotlin.multiplatform") {
-        apply(plugin = "org.jmailen.kotlinter")
-        apply(plugin = "dev.detekt")
+    extensions.configure<DetektExtension>("detekt") {
+        config.setFrom(rootProject.files("detekt.yml"))
+    }
 
+    plugins.withId("org.jetbrains.kotlin.multiplatform") {
         tasks.matching { it.name.endsWith("BrowserTest") }.configureEach {
             onlyIf {
                 providers.gradleProperty("fiktion.enableBrowserTests").getOrElse("false").toBoolean()
@@ -36,17 +42,7 @@ subprojects {
         }
 
         extensions.configure<DetektExtension>("detekt") {
-            config.setFrom(rootProject.files("detekt.yml"))
             source.from("src")
-        }
-    }
-
-    plugins.withId("org.jetbrains.kotlin.jvm") {
-        apply(plugin = "org.jmailen.kotlinter")
-        apply(plugin = "dev.detekt")
-
-        extensions.configure<DetektExtension>("detekt") {
-            config.setFrom(rootProject.files("detekt.yml"))
         }
     }
 }
