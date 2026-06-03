8f19ab3ae2b97e37201ef645879e997f4744553f
sya-ri <git@s7a.dev>
2026-06-03 22:58:43 +0900

    Update Kotlin to 2.4.0

--- fiktion-addon-arrow-core/build.gradle.kts
+++ fiktion-addon-arrow-core/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
+
 import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.KotlinMultiplatform
 import com.vanniktech.maven.publish.SourcesJar
